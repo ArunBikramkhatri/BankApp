23ed6b6847ea740661dfefddef87fdca69251b6a
Arun_Bikram_Khatri <khatriarun221@gmail.com>
2022-12-08 20:20:43 +0545

    a

--- src/main/java/com/bankapplication/Controller/BankController.java
+++ src/main/java/com/bankapplication/Controller/BankController.java
@@ -1,4 +1,16 @@
 package com.bankapplication.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/e-bank/")
 public class BankController {
+
+    @GetMapping("home")
+    public String homepage(){
+        return "homepage";
+    }
+
 }
