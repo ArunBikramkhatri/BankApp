347931a496bfcd878db91093ba5ab7b7437c2135
Arun_Bikram_Khatri <khatriarun221@gmail.com>
2022-12-12 21:56:43 +0545

    a

---
+++ src/main/java/com/bankapplication/Config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.bankapplication.Config;
+
+
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@EnableWebSecurity
+public interface  SecurityConfig extends WebSecurityConfiguration{
+
+}
